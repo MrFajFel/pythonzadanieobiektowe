52be0d37609462200b432e5fbef6649d00a61947
Student <elkadjk@.pl>
2024-05-14 15:05:04 +0200

    Pierwszy commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from Przedmiot import Przedmiot
+from Student import Student
+
+if __name__ == '__main__':
+    przedmiot = Przedmiot('bazy danych')
+    uczen = Student('Maciek','Wow','007','2022')
+    przedmiot.set_ects(10)
+    uczen.dodaj_przedmiot(przedmiot)
